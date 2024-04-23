4bf96d2d0b096d013f893f96d2feba6eb81f5218
jobayer-rahman <jobayer_rahman@yahoo.com>
2024-04-23 18:58:20 +0600

    Added breadcrumb, available_sizes, general_description to the product details.

--- main.py
+++ main.py
@@ -15,6 +15,28 @@ def get_details(page_url, tag, selector):
         return ""
 
 
+def get_available_size(page_url, tag, selector):
+    try:
+        resp = requests.get(page_url)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        size = soup.find(tag, class_=selector)
+        all_sizes = ' '.join(size.stripped_strings)
+        return all_sizes
+    except Exception:
+        return ""
+
+
+def get_breadcrumbs(page_url, tag, selector):
+    try:
+        resp = requests.get(page_url)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        bread = soup.find(tag, class_=selector)
+        breadcrumbs = [item.text.strip() for item in bread.find_all("li")]
+        return ' '.join(breadcrumbs)
+    except Exception:
+        return ""
+
+
 def men_category():
     resp = requests.get(page_url)
     soup = BeautifulSoup(resp.text, 'html.parser')
@@ -28,10 +50,13 @@ def product_details(product_urls):
     for product_url in product_urls:
         details = {
             'url': product_url,
+            'breadcrumb': get_breadcrumbs(product_url, 'ul', 'breadcrumbList'),
             'category': get_details(product_url, 'a', 'groupName'),
             'product_name': get_details(product_url, 'h1', 'itemTitle'),
             'price': get_details(product_url, 'span', 'price-value'),
-            'title_of_description': get_details(product_url, 'h4', 'itemFeature')
+            'available_sizes': get_available_size(product_url, 'ul', 'sizeSelectorList'),
+            'title_description': get_details(product_url, 'h4', 'itemFeature'),
+            'general_description': get_details(product_url, 'div', 'commentItem-mainText'),
         }
         print(details)
         # print(product_url)
